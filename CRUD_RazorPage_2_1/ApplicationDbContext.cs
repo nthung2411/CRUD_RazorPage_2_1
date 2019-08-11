fb4f2167be3a8e4a511ba6220e85748728b4ffe7
Robert Hung Nguyen <nthung2411@gmail.com>
2019-08-11 11:27:00 +0700

    first commit

---
+++ CRUD_RazorPage_2_1/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_RazorPage_2_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_RazorPage_2_1
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Tech Stacks
+- DotNet Core 2.2
+- Entity Framework Core
+- Postgre SQL integrate
